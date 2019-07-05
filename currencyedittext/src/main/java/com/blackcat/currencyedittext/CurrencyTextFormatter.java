0fd0850166e4b66138352b11117c12c075159bbd
Daniel Freitas <daniielf.13@gmail.com>
2019-07-05 18:18:54 -0300

    Update Format
    Changed the format to remove Currency symbol and replace '.' to ','

--- currencyedittext/src/main/java/com/blackcat/currencyedittext/CurrencyTextFormatter.java
+++ currencyedittext/src/main/java/com/blackcat/currencyedittext/CurrencyTextFormatter.java
@@ -85,7 +85,7 @@ public final class CurrencyTextFormatter {
         else {
             throw new IllegalArgumentException("Invalid amount of digits found (either zero or too many) in argument val");
         }
-        return val;
+        return value.substring(1).replaceAll('.', ',');
     }
 
 }
